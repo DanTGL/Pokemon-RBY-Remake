7c0afe7bed440404ecc5f14fdfbe04e8dc5e16e1
DanTGL <33489389+DanTGL@users.noreply.github.com>
2021-02-02 16:04:15 +0100

    Initial commit

---
+++ Assets/Scripts/Battle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Battle : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    int GetRandomMove() {
+        int rand = UnityEngine.Random.Range(0, 255);
+
+        if (rand < 0x3F) {
+            return 0;
+        } else if (rand < 0x7E) {
+            return 1;
+        } else if (rand < 0xBD) {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+
+}
