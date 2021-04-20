cd9a4bced65707e862a1c101066e4851044bc772
DanTGL <33489389+DanTGL@users.noreply.github.com>
2021-04-20 16:08:03 +0200

    Updated Pokemon class

--- Assets/Scripts/Pokemon.cs
+++ Assets/Scripts/Pokemon.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pokemon : MonoBehaviour {
+public class Pokemon {
 
     public Move[] moves;
     public int hp;
+    public int maxHp;
     public int attack;
     public int defense;
     public int speed = 5;
 
     public int level = 1;
+
+    public Pokemon(int hp, int attack, int defense, int speed, int level) {
+        this.hp = hp;
+        this.maxHp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.level = level;
+
+        moves = new Move[4];
+        for (int i = 0; i < 4; i++) {
+            moves[i] = new Move(UnityEngine.Random.Range(1, 50));
+        }
+    }
 
     public void SetHP(int hp) {
         this.hp = hp;
